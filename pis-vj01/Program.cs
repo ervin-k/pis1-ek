ca967f5a1b6be9c60c173d2f7aeb72c2c775ef85
student <student@B420-02>
2021-03-25 18:33:55 +0100

    prva promjena

--- pis-vj01/Program.cs
+++ pis-vj01/Program.cs
@@ -7,6 +7,7 @@ namespace pis_vj01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Nesto novo");
         }
     }
 }
